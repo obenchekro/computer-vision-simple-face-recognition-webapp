b8306b83b9c764d44620244f655f454c8bce70da
Othmane BENCHEKROUN <75435299+obenchekro@users.noreply.github.com>
2023-01-27 02:28:26 +0100

    Update camera_rendering.py
    quick fix on capture function

--- app/camera_rendering.py
+++ app/camera_rendering.py
@@ -23,34 +23,23 @@ def launch_camera():
             raise Exception("Encoding failed")
             
     camera.release()
+     
     
 def capture(filepath):
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
     cv2.rectangle(frame, (200, 200), (400, 400), (0, 0, 255), 2)
     filename = '{}.jpg'.format(str(uuid.uuid4()))
-    cv2.imwrite(os.path.join(os.path.abspath(filepath), filename), frame)
-    camera.release()
-    return os.path.join(os.path.abspath(filepath), filename)
+    
+    if frame is not None and not frame.size == 0:
+        cv2.imwrite(os.path.join(os.path.abspath(filepath), filename), frame)
+        camera.release()
+        return os.path.join(os.path.abspath(filepath), filename)
+    else:
+        raise Exception("Invalid image received")
 
 def is_human_face(filepath):
     image = cv2.imread(filepath)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
     return True if len(faces) else False
-
-
-def capture1(filepath):
-    camera = cv2.VideoCapture(0)
-    ret, frame = camera.read()
-    ret, buffer = cv2.imencode('.jpg', frame)
-    if ret:
-        filename = '{}.jpg'.format(str(uuid.uuid4()))
-        cv2.imwrite(os.path.join(filepath, filename), frame)
-        camera.release()
-        return filename
-    else:
-        raise Exception("Error capturing photo")
-
-#capture1('/static/uploads')
-print(is_human_face(r'D:\Games\Hitman 3\ent\ai.jpg'))
